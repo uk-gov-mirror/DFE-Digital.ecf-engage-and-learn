fc345d3ac62fbef7b03dee627d486b0826152943
Michal Kreft <michal.kreft@softwire.com>
2021-02-17 15:41:55 +0000

    Add welcome email templates

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,8 @@
 
 class UserMailer < ApplicationMailer
   SIGN_IN_EMAIL_TEMPLATE = "7ab8db5b-9842-4bc3-8dbb-f590a3198d9e"
-  EMAIL_CONFIRMATION_TEMPLATE = "50059d26-c65d-4e88-831a-8bfb9f4116cd"
-  PRIMARY_CONTACT_TEMPLATE = "a7cc4d19-c0cb-4187-a71b-1b1ea029924f"
+  ECT_WELCOME_TEMPLATE = "147031f0-cd41-483c-a679-13bc3fd0d4f5"
+  MENTOR_WELCOME_TEMPLATE = "252333c2-1785-45cf-bd11-24dc61fd8c49"
 
   def sign_in_email(user, url)
     template_mail(
@@ -18,33 +18,28 @@ class UserMailer < ApplicationMailer
     )
   end
 
-  def confirmation_instructions(user, token, _options = {})
-    confirmation_url = Rails.application.routes.url_helpers.user_confirmation_url(
-      confirmation_token: token,
-      host: Rails.application.config.domain,
-    )
-
+  def early_career_teacher_welcome(user, url)
     template_mail(
-      EMAIL_CONFIRMATION_TEMPLATE,
+      ECT_WELCOME_TEMPLATE,
       to: user.email,
       rails_mailer: mailer_name,
       rails_mail_template: action_name,
       personalisation: {
         full_name: user.full_name,
-        confirmation_url: confirmation_url,
+        sign_in_url: url,
       },
     )
   end
 
-  def primary_contact_notification(coordinator, school)
+  def mentor_welcome(user, url)
     template_mail(
-      PRIMARY_CONTACT_TEMPLATE,
-      to: school.primary_contact_email,
+      MENTOR_WELCOME_TEMPLATE,
+      to: user.email,
       rails_mailer: mailer_name,
       rails_mail_template: action_name,
       personalisation: {
-        coordinator_full_name: coordinator.full_name,
-        coordinator_email_address: coordinator.email,
+        full_name: user.full_name,
+        sign_in_url: url,
       },
     )
   end
