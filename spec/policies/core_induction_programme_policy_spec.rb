bf780f0ae0433978ab7400ba032f0e0fd21ecb3e
Steven <steven.woods@digital.education.gov.uk>
2021-04-08 18:49:14 +0100

    Update cip request and policy spec tests

--- spec/policies/core_induction_programme_policy_spec.rb
+++ spec/policies/core_induction_programme_policy_spec.rb
@@ -6,9 +6,21 @@ RSpec.describe CoreInductionProgrammePolicy, type: :policy do
   subject { described_class.new(user, core_induction_programme) }
   let(:core_induction_programme) { create(:core_induction_programme) }
 
-  context "accessing show as early career teacher" do
-    let(:user) { create(:user, :early_career_teacher, { core_induction_programme: core_induction_programme }) }
-
+  context "accessing cip views as admin" do
+    let(:user) { create(:user, :admin) }
     it { is_expected.to permit_action(:show) }
+    it { is_expected.to permit_action(:index) }
+  end
+
+  context "accessing cip views as an early career teacher" do
+    let(:user) { create(:user, :early_career_teacher, { core_induction_programme: core_induction_programme }) }
+    it { is_expected.to permit_action(:show) }
+    it { is_expected.not_to permit_action(:index) }
+  end
+
+  context "being a visitor" do
+    let(:user) { nil }
+    it { is_expected.not_to permit_action(:show) }
+    it { is_expected.not_to permit_action(:index) }
   end
 end
