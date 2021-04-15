95e5ebdf82d3c630846510588579f37fcd87d270
Michal Kreft <michal.kreft@softwire.com>
2021-04-15 17:03:18 +0100

    Update factory trait definition to not break github actions

--- spec/factories/course_module.rb
+++ spec/factories/course_module.rb
@@ -2,15 +2,13 @@
 
 FactoryBot.define do
   factory :course_module do
-    year = CourseYear.new(title: "Test Course year", content: "No content", core_induction_programme_one: FactoryBot.create(:core_induction_programme))
-
     title { "Test Course module" }
     content { "No content" }
-    course_year { year }
+    course_year { FactoryBot.create(:course_year) }
 
     trait :with_previous do
-      previous_module do
-        FactoryBot.create(:course_module, course_year: year, title: "Test previous module")
+      after :build do |course_module|
+        course_module.previous_module = FactoryBot.create(:course_module, course_year: course_module.year, title: "Test previous module")
       end
     end
   end
