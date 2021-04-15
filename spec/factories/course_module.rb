1557e076a128139993ba274cb5758f59b7016d84
Michal Kreft <michal.kreft@softwire.com>
2021-04-15 16:59:03 +0100

    Clever fix one

--- spec/factories/course_module.rb
+++ spec/factories/course_module.rb
@@ -1,18 +1,14 @@
 # frozen_string_literal: true
 
-year = CourseYear.new(title: "Test Course year", content: "No content", core_induction_programme_one: FactoryBot.create(:core_induction_programme))
-
 FactoryBot.define do
   factory :course_module do
     title { "Test Course module" }
     content { "No content" }
     course_year { FactoryBot.create(:course_year) }
 
-    course_year { year }
-
     trait :with_previous do
-      previous_module do
-        FactoryBot.create(:course_module, course_year: year, title: "Test previous module")
+      after :build do |course_module|
+        course_module.previous_module = FactoryBot.create(:course_module, course_year: course_module.year, title: "Test previous module")
       end
     end
   end
