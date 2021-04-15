efb439d0d82fd19ccb99d66ee037568ba5a91f5a
Michal Kreft <michal.kreft@softwire.com>
2021-04-15 16:54:19 +0100

    Silly fix two

--- spec/factories/course_module.rb
+++ spec/factories/course_module.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-FactoryBot.define do
-  year = CourseYear.new(title: "Test Course year", content: "No content", core_induction_programme_one: FactoryBot.create(:core_induction_programme))
+year = CourseYear.new(title: "Test Course year", content: "No content", core_induction_programme_one: FactoryBot.create(:core_induction_programme))
 
+FactoryBot.define do
   factory :course_module do
     title { "Test Course module" }
     content { "No content" }
