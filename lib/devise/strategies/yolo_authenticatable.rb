f6f07b8d8cb7a23aa0a12946effb5b8fd68b3cd7
Michal Kreft <michal.kreft@softwire.com>
2021-04-15 15:40:30 +0100

    Silly change

--- lib/devise/strategies/yolo_authenticatable.rb
+++ lib/devise/strategies/yolo_authenticatable.rb
@@ -15,6 +15,7 @@ module Devise
             errors.add :email, "Enter the email address your school used when they created your account"
             fail! "This email address is not the same as the one your school used to create your account."
           end
+
         end
       end
     end
