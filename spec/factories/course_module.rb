5f08da5c83c83cef21b7edcb40c95dec9e1c2b06
Michal Kreft <michal.kreft@softwire.com>
2021-04-15 16:41:11 +0100

    Binary search one

--- spec/factories/course_module.rb
+++ spec/factories/course_module.rb
@@ -2,16 +2,8 @@
 
 FactoryBot.define do
   factory :course_module do
-    year = CourseYear.new(title: "Test Course year", content: "No content", core_induction_programme_one: FactoryBot.create(:core_induction_programme))
-
     title { "Test Course module" }
     content { "No content" }
-    course_year { year }
-
-    trait :with_previous do
-      previous_module do
-        FactoryBot.create(:course_module, course_year: year, title: "Test previous module")
-      end
-    end
+    course_year { FactoryBot.create(:course_year) }
   end
 end
